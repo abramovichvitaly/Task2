f55e75a2522189fd528deae91d1eedceb78adf10
Abramovichvitaly <abramovich1997@yandex.ru>
2017-09-19 16:23:36 +0700

    День рождения

---
+++ task2.php
@@ -0,0 +1,15 @@
+<?php
+$myYear=1997;
+$myMonth=5;
+$myDay=3;
+$currentYear=2017;
+$currentMonth=9;
+$currentDay=19;
+$myAge="x";
+if ($myMonth < $currentMonth || ($myMonth = $currentMonth && $myDay < $currentDay) ){
+	$myAge = $currentYear - $myYear;
+} 	echo $myAge {
+	else echo"дня родждения еще не было"
+}
+?>
+
